86949cb046bb7f9002e227a07d1705ad6eb5343b
Scott McCormack <scott.mccormack@articulate.com>
2017-08-25 13:48:27 -0400

    Adjust to prevent buble probs

--- src/promisify.js
+++ src/promisify.js
@@ -1,7 +1,8 @@
 // promisify : ((a..., b -> ()) -> (), c) -> a... -> Promise b
 const promisify = (f, ctx) => (...args) =>
   new Promise((res, rej) => {
-    f.call(ctx, ...args, (err, val) => err ? rej(err) : res(val))
+    const cb = (err, val) => err ? rej(err) : res(val)
+    f.call(ctx, ...args, cb)
   })
 
 module.exports = promisify
