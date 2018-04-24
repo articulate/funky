5b3165bad2a312066a66342e56665abfdb506a46
Markus Greystone <markus.greystone@gmail.com>
2018-04-24 13:27:03 -0400

    `mapP` over objects

--- test/mapP.js
+++ test/mapP.js
@@ -7,11 +7,23 @@ const { mapP } = require('..')
 describe('mapP', () => {
   const res = property()
 
-  beforeEach(() =>
-    mapP(add(1), [ 1, 2, 3 ]).then(res)
-  )
+  context('an array argument', () => {
+    beforeEach(() =>
+      mapP(add(1), [ 1, 2, 3 ]).then(res)
+    )
 
-  it('maps over a list with an async function', () =>
-    expect(res()).to.eql([ 2, 3, 4 ])
-  )
+    it('maps over a list with an async function', () =>
+      expect(res()).to.eql([ 2, 3, 4 ])
+    )
+  })
+
+  context('an object argument', () => {
+    beforeEach(() =>
+      mapP(add(1), { foo: 1, bar: 2, baz: 3 }).then(res)
+    )
+
+    it('maps over a list with an async function', () =>
+      expect(res()).to.eql({ foo: 2, bar: 3, baz: 4 })
+    )
+  })
 })
