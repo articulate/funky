1827dcba777bf8c44bd8e4b5fdada93185d97b5e
Markus Greystone <markus.greystone@gmail.com>
2018-04-25 15:09:04 -0400

    `mapP` over objects (#11)

--- src/mapP.js
+++ src/mapP.js
@@ -1,8 +1,30 @@
 const curry = require('ramda/src/curry')
 const map   = require('ramda/src/map')
 
-// mapP :: Functor f => (a -> Promise b) -> f a -> Promise f b
-const mapP = (f, list) =>
+const mapArrayP = (f, list) =>
   Promise.all(map(f, list))
+
+const mapObjectP = (f, list) => {
+  const result = {}
+  const promises = []
+
+  for (const key in list) {
+    const p = f(list[key]).then(val => { result[key] = val })
+    promises.push(p)
+  }
+
+  return Promise.all(promises).then(() => result)
+}
+
+// mapP :: Functor f => (a -> Promise b) -> f a -> Promise f b
+const mapP = (f, list) => {
+  switch (Object.prototype.toString.call(list)) {
+    case '[object Object]':
+      return mapObjectP(f, list)
+
+    default:
+      return mapArrayP(f, list)
+  }
+}
 
 module.exports = curry(mapP)
