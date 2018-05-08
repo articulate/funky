56d101b5a01817faa7ef0a74623c5276df77dc5c
Scott McCormack <scott.mccormack@articulate.com>
2018-05-08 12:46:04 -0400

    Add spy

--- test/batch.js
+++ test/batch.js
@@ -1,16 +1,21 @@
 const { expect } = require('chai')
 const property   = require('prop-factory')
+const Spy        = require('@articulate/spy')
 
 const { batch, evolveP } = require('..')
 
 describe('batch', () => {
-  const res = property()
-
   const obj = { a: 'a', b: 'b', c: 'c' }
+  const res = property()
+  const spy = Spy()
+
+  beforeEach(() =>
+    spy.reset()
+  )
 
   describe('with no options', () => {
     const asyncListFn = ([a, b, c]) =>
-      Promise.resolve([a, b, c])
+      (spy(), Promise.resolve([a, b, c]))
 
     const batched = batch(undefined, asyncListFn)
 
@@ -24,14 +29,15 @@ describe('batch', () => {
       test(obj).then(res)
     )
 
-    it('has safe defaults', () =>
+    it('has safe defaults', () => {
       expect(res()).to.eql(obj)
-    )
+      expect(spy.calls.length).to.equal(1)
+    })
   })
 
   describe('with a batch limit', () => {
     const asyncListFn = ({ length }) =>
-      Promise.resolve(Array(length).fill(length))
+      (spy(), Promise.resolve(Array(length).fill(length)))
 
     const batched = batch({ limit: 2 }, asyncListFn)
 
@@ -45,14 +51,15 @@ describe('batch', () => {
       test(obj).then(res)
     )
 
-    it('limits the batch length', () =>
+    it('limits the batch length', () => {
       expect(res()).to.eql({ a: 2, b: 2, c: 1 })
-    )
+      expect(spy.calls.length).to.equal(2)
+    })
   })
 
   describe('with a max wait', () => {
     const asyncListFn = ({ length }) =>
-      Promise.resolve(Array(length).fill(length))
+      (spy(), Promise.resolve(Array(length).fill(length)))
 
     const batched = batch({ wait: 16 }, asyncListFn)
 
@@ -73,8 +80,9 @@ describe('batch', () => {
       test(obj).then(res)
     )
 
-    it('throttles the batches', () =>
+    it('throttles the batches', () => {
       expect(res()).to.eql({ a: 2, b: 2, c: 1 })
-    )
+      expect(spy.calls.length).to.equal(2)
+    })
   })
 })
