d4f778e0eb2d136123447c780ef5c1782a925e1a
Robert Pearce <me@robertwpearce.com>
2018-09-19 05:43:44 +1200

    fix assocWith & assocWithP currying (#20)

--- src/assocWith.js
+++ src/assocWith.js
@@ -4,7 +4,7 @@ const assoc    = require('ramda/src/assoc')
 const identity = require('ramda/src/identity')
 
 // assocWith :: String -> ({ k: v } -> a) -> { k: v } -> { k: v }
-const assocWith = (key, fn) =>
-  converge(assoc(key), [fn, identity])
+const assocWith = (key, fn, obj) =>
+  converge(assoc(key), [fn, identity])(obj)
 
 module.exports = curry(assocWith)
