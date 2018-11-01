a10befdf52ccd55ba17522e5af637225558ef632
Robert Pearce <me@robertwpearce.com>
2018-11-02 08:20:29 +1300

    redo evolveP tests

--- test/evolveP.js
+++ test/evolveP.js
@@ -1,36 +1,21 @@
 const { expect } = require('chai')
-const property   = require('prop-factory')
 
 const { add, mult } = require('./lib/async')
 const { evolveP }   = require('..')
 
+const arg1 = { foo: add(2), bar: { baz: mult(3) } }
+const arg2 = { foo: 1, bar: { baz: 1 }, bat: 1 }
+
 describe('evolveP', () => {
-  context('when partially applied', () => {
-    const res = property()
+  it('recursively evolves an async shallow copy (??) of an object', () =>
+    evolveP(arg1, arg2).then(res => {
+      expect(res).to.eql({ foo: 3, bar: { baz: 3 }, bat: 1 })
+    })
+  )
 
-    beforeEach(() =>
-      evolveP(
-        { foo: add(2), bar: { baz: mult(3) } }
-      )({ foo: 1, bar: { baz: 1 }, bat: 1 }).then(res)
-    )
-
-    it('recursively evolves an async shallow copy (??) of an object', () =>
-      expect(res()).to.eql({ foo: 3, bar: { baz: 3 }, bat: 1 })
-    )
-  })
-
-  context('when all arguments provided', () => {
-    const res = property()
-
-    beforeEach(() =>
-      evolveP(
-        { foo: add(2), bar: { baz: mult(3) } },
-        { foo: 1, bar: { baz: 1 }, bat: 1 }
-      ).then(res)
-    )
-
-    it('recursively evolves an async shallow copy (??) of an object', () =>
-      expect(res()).to.eql({ foo: 3, bar: { baz: 3 }, bat: 1 })
-    )
-  })
+  it('is curried', () =>
+    evolveP(arg1)(arg2).then(res => {
+      expect(res).to.eql({ foo: 3, bar: { baz: 3 }, bat: 1 })
+    })
+  )
 })
