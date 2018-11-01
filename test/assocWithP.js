aa635b5b63801d2c5ac43adb54823dc0a224d757
Robert Pearce <me@robertwpearce.com>
2018-11-02 08:24:14 +1300

    arity testing: assocWithP

--- test/assocWithP.js
+++ test/assocWithP.js
@@ -1,33 +1,26 @@
 const { expect } = require('chai')
-const property   = require('prop-factory')
 
 const { assocWithP } = require('..')
 
 const concatBarToFoo = obj =>
   Promise.resolve(obj.foo + 'bar')
 
-describe('assocWith', () => {
-  context('when partially applied', () => {
-    const res = property()
+describe('assocWithP', () => {
+  it('sets the foo property to the result of the function', () =>
+    assocWithP('baz', concatBarToFoo, { foo: 'foo' }).then(res => {
+      expect(res).to.eql({ foo: 'foo', baz: 'foobar' })
+    })
+  )
 
-    beforeEach(() =>
-      assocWithP('baz', concatBarToFoo)({ foo: 'foo' }).then(res)
-    )
+  it('is curried, arity 1', () =>
+    assocWithP('baz', concatBarToFoo)({ foo: 'foo' }).then(res => {
+      expect(res).to.eql({ foo: 'foo', baz: 'foobar' })
+    })
+  )
 
-    it('sets the foo property to the result of the function', () =>
-      expect(res()).to.eql({ foo: 'foo', baz: 'foobar' })
-    )
-  })
-
-  context('when all arguments provided', () => {
-    const res = property()
-
-    beforeEach(() =>
-      assocWithP('baz', concatBarToFoo, { foo: 'foo' }).then(res)
-    )
-
-    it('sets the foo property to the result of the function', () =>
-      expect(res()).to.eql({ foo: 'foo', baz: 'foobar' })
-    )
-  })
+  it('is curried, arity 2, unary', () =>
+    assocWithP('baz')(concatBarToFoo)({ foo: 'foo' }).then(res => {
+      expect(res).to.eql({ foo: 'foo', baz: 'foobar' })
+    })
+  )
 })
