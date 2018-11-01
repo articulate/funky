3e2bb0868b8518dc5a764d4233fd7e614f73c3d6
Robert Pearce <me@robertwpearce.com>
2018-11-02 08:02:07 +1300

    arity testing: combineWith

--- test/combineWith.js
+++ test/combineWith.js
@@ -6,14 +6,11 @@ const { combineWith } = require('..')
 
 describe('combineWith', () => {
   it('combines with the results of the function', () =>
-    expect(
-      combineWith(multiply, add(2), 3)
-    ).to.eql(15)
+    expect(combineWith(multiply, add(2), 3)).to.eql(15)
   )
 
-  it('is curried', () =>
-    expect(
-      combineWith(multiply, add(2))(3)
-    ).to.eql(15)
-  )
+  it('is curried', () => {
+    expect(combineWith(multiply, add(2))(3)).to.eql(15)
+    expect(combineWith(multiply)(add(2))(3)).to.eql(15)
+  })
 })
