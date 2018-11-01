517b60a19dae59902b88ab2a6cf43a1ef4b24622
Robert Pearce <me@robertwpearce.com>
2018-11-02 07:45:38 +1300

    arity testing: assocWith

--- test/assocWith.js
+++ test/assocWith.js
@@ -6,19 +6,16 @@ const concatBarToFoo = obj =>
   obj.foo + 'bar'
 
 describe('assocWith', () => {
-  context('when partially applied', () => {
-    it('sets the foo property to the result of the function', () =>
-      expect(
-        assocWith('baz', concatBarToFoo)({ foo: 'foo' })
-      ).to.eql({ foo: 'foo', baz: 'foobar' })
-    )
-  })
+  it('sets the foo property to the result of the function', () =>
+    expect(assocWith('baz', concatBarToFoo, { foo: 'foo' }))
+      .to.eql({ foo: 'foo', baz: 'foobar' })
+  )
 
-  context('when all arguments provided', () => {
-    it('sets the foo property to the result of the function', () =>
-      expect(
-        assocWith('baz', concatBarToFoo, { foo: 'foo' })
-      ).to.eql({ foo: 'foo', baz: 'foobar' })
-    )
+  it('is curried', () => {
+    expect(assocWith('baz', concatBarToFoo)({ foo: 'foo' }))
+      .to.eql({ foo: 'foo', baz: 'foobar' })
+
+    expect(assocWith('baz')(concatBarToFoo)({ foo: 'foo' }))
+      .to.eql({ foo: 'foo', baz: 'foobar' })
   })
 })
