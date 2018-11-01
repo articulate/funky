05acf739563507a51af5b0669c07ff06b9d5866f
Robert Pearce <me@robertwpearce.com>
2018-11-02 08:32:15 +1300

    get combine tests style consistent

--- test/combine.js
+++ test/combine.js
@@ -5,14 +5,12 @@ const { combine } = require('..')
 
 describe('combine', () => {
   it('merges with the results of the function', () =>
-    expect(
-      combine(always({ foo: 'bar' }), { baz: 'bip' })
-    ).to.eql({ foo: 'bar', baz: 'bip' })
+    expect(combine(always({ foo: 'bar' }), { baz: 'bip' }))
+      .to.eql({ foo: 'bar', baz: 'bip' })
   )
 
   it('is curried', () =>
-    expect(
-      combine(always({ foo: 'bar' }))({ baz: 'bip' })
-    ).to.eql({ foo: 'bar', baz: 'bip' })
+    expect(combine(always({ foo: 'bar' }))({ baz: 'bip' }))
+      .to.eql({ foo: 'bar', baz: 'bip' })
   )
 })
