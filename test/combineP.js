aa582fabc8742d5b0400dfe9f157345a6fb39305
Robert Pearce <me@robertwpearce.com>
2018-11-02 08:00:45 +1300

    redo combineP tests

--- test/combineP.js
+++ test/combineP.js
@@ -1,39 +1,26 @@
 const { expect } = require('chai')
-const property   = require('prop-factory')
 const always     = require('ramda/src/always')
 
 const { combineP } = require('..')
 
 const combineFn = always(Promise.resolve({ foo: 'bar' }))
 
-describe('combine', () => {
-  context('when partially applied', () => {
-    const res = property()
-
-    beforeEach(() =>
-      combineP(combineFn)({ baz: 'bip' }).then(res)
-    )
-
-    it('merges with the results of the function', () =>
-      expect(res()).to.eql({
+describe('combineP', () => {
+  it('merges with the results of the function', () =>
+    combineP(combineFn, { baz: 'bip' }).then(res => {
+      expect(res).to.eql({
         foo: 'bar',
         baz: 'bip',
       })
-    )
-  })
+    })
+  )
 
-  context('when all arguments provided', () => {
-    const res = property()
-
-    beforeEach(() =>
-      combineP(combineFn, { baz: 'bip' }).then(res)
-    )
-
-    it('merges with the results of the function', () =>
-      expect(res()).to.eql({
+  it('is curried', () =>
+    combineP(combineFn)({ baz: 'bip' }).then(res => {
+      expect(res).to.eql({
         foo: 'bar',
         baz: 'bip',
       })
-    )
-  })
+    })
+  )
 })
