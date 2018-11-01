3dadaf2297f8be57f7c6893e3b5cccc9a69a5895
Robert Pearce <me@robertwpearce.com>
2018-11-02 08:31:11 +1300

    arity testing: useWithP

--- test/useWithP.js
+++ test/useWithP.js
@@ -1,17 +1,18 @@
 const { expect } = require('chai')
-const property   = require('prop-factory')
 
 const { add, mult } = require('./lib/async')
 const { useWithP } = require('..')
 
 describe('useWithP', () => {
-  const res = property()
-
-  beforeEach(() =>
-    useWithP(mult, [ add(1), add(2) ])(3)(4).then(res)
+  it('transforms arguments with async functions', () =>
+    useWithP(mult, [ add(1), add(2) ])(3, 4).then(res => {
+      expect(res).to.equal(24) // (3 + 1) * (4 + 2)
+    })
   )
 
-  it('transforms arguments with async functions', () =>
-    expect(res()).to.equal(24) // (3 + 1) * (4 + 2)
+  it('is curried', () =>
+    useWithP(mult)([ add(1), add(2) ])(3)(4).then(res => {
+      expect(res).to.equal(24) // (3 + 1) * (4 + 2)
+    })
   )
 })
