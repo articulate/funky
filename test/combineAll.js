3da79bb08ea4ea848e9aaa5f2b598ed87eb5a49c
Robert Pearce <me@robertwpearce.com>
2018-11-02 08:35:32 +1300

    add clarity to combineAll tests

--- test/combineAll.js
+++ test/combineAll.js
@@ -3,22 +3,19 @@ const always     = require('ramda/src/always')
 
 const { combineAll } = require('..')
 
+const actions = [
+  always({ foo: 1 }),
+  always({ bar: 2 }),
+]
+
 describe('combineAll', () => {
   it('combines the results of all functions', () =>
-    expect(
-      combineAll([
-        always({ foo: 1 }),
-        always({ bar: 2 }),
-      ], { baz: 3 })
-    ).to.eql({ foo: 1, bar: 2, baz: 3 })
+    expect(combineAll(actions, { baz: 3 }))
+      .to.eql({ foo: 1, bar: 2, baz: 3 })
   )
 
   it('is curried', () =>
-    expect(
-      combineAll([
-        always({ foo: 1 }),
-        always({ bar: 2 }),
-      ])({ baz: 3 })
-    ).to.eql({ foo: 1, bar: 2, baz: 3 })
+    expect(combineAll(actions)({ baz: 3 }))
+      .to.eql({ foo: 1, bar: 2, baz: 3 })
   )
 })
