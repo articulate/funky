f139f9dddbf3f5b63467d8e667a1e090d5cb7c5f
James Adams <jadams@articulate.com>
2019-01-25 14:46:05 -0600

    Expand tests

--- test/onSuccessP.js
+++ test/onSuccessP.js
@@ -11,10 +11,54 @@ describe('onSuccessP', () => {
     spy2.reset()
   })
 
-  it('executes second function when first resolves, returns output of first function', () => {
+  it('executes promise returning second function after promise returning first function resolves', () => {
     const afterThis = a => Promise.resolve(a)
       .then(spy1)
       .then(() => 'b')
+
+    const doThat = a => Promise.resolve(a)
+      .then(spy2)
+      .then(() => 'c')
+
+    return onSuccessP(afterThis, doThat, 'a')
+      .then(result => {
+        expect(result).to.eql('b')
+
+        expect(spy1.calls.length).to.eql(1)
+        expect(spy1.calls[0]).to.eql([ 'a' ])
+
+        expect(spy2.calls.length).to.eql(1)
+        expect(spy2.calls[0]).to.eql([ 'a' ])
+      })
+  })
+
+  it('executes non-promise returning second function after promise returning first function resolves', () => {
+    const afterThis = a => Promise.resolve(a)
+      .then(spy1)
+      .then(() => 'b')
+
+    const doThat = a => {
+      spy2(a)
+      return 'c'
+    }
+
+    return onSuccessP(afterThis, doThat, 'a')
+      .then(result => {
+        expect(result).to.eql('b')
+
+        expect(spy1.calls.length).to.eql(1)
+        expect(spy1.calls[0]).to.eql([ 'a' ])
+
+        expect(spy2.calls.length).to.eql(1)
+        expect(spy2.calls[0]).to.eql([ 'a' ])
+      })
+  })
+
+  it('executes promise returning second function after non-promise returning first function returns', () => {
+    const afterThis = a => {
+      spy1(a)
+      return 'b'
+    }
 
     const doThat = a => Promise.resolve(a)
       .then(spy2)
@@ -55,7 +99,7 @@ describe('onSuccessP', () => {
       })
   })
 
-  it('does not execute second function when first rejects', done => {
+  it('does not execute promise returning second function when promise returning first function rejects', done => {
     const error = new Error('b')
 
     const afterThis = a => Promise.resolve(a)
@@ -78,5 +122,58 @@ describe('onSuccessP', () => {
 
         done()
       })
+  })
+
+  it('does not execute non-promise returning second function when promise returning first function rejects', done => {
+    const error = new Error('b')
+
+    const afterThis = a => Promise.resolve(a)
+      .then(spy1)
+      .then(() => { throw error })
+
+    const doThat = a => {
+      spy2(a)
+      return 'c'
+    }
+
+    onSuccessP(afterThis, doThat, 'a')
+      .then(() => done('Unexpected Success'))
+      .catch(e => {
+        expect(e).to.eql(error)
+
+        expect(spy1.calls.length).to.eql(1)
+        expect(spy1.calls[0]).to.eql([ 'a' ])
+
+        expect(spy2.calls.length).to.eql(0)
+
+        done()
+      })
+  })
+
+  it('does not execute promise returning second function when non-promise returning first function throws', done => {
+    const error = new Error('b')
+
+    const afterThis = a => {
+      spy1(a)
+      throw error
+    }
+
+    const doThat = a => Promise.resolve(a)
+      .then(spy2)
+      .then(() => 'c')
+
+    try {
+      onSuccessP(afterThis, doThat, 'a')
+        .then(() => done('Unexpected Success'))
+    } catch (e) {
+      expect(e).to.eql(error)
+
+      expect(spy1.calls.length).to.eql(1)
+      expect(spy1.calls[0]).to.eql([ 'a' ])
+
+      expect(spy2.calls.length).to.eql(0)
+
+      done()
+    }
   })
 })
