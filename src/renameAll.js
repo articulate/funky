15b49bd5972e345a8c73504159202e73edc62977
Jessica Poémape (she/her) <jpoemape@articulate.com>
2020-09-10 16:43:13 -0600

    renameAll deleting keys (#39)
    * fixing renameAll to allow a from and a to to have the same name
    
    * not interating twice

--- src/renameAll.js
+++ src/renameAll.js
@@ -2,22 +2,16 @@ const curry = require('ramda/src/curry')
 
 // renameAll :: { k: v } -> { k: v } -> { k: v }
 const renameAll = (renames, obj) => {
-  obj = Object.assign({}, obj)
-
-  for (let frum in renames) {
-    if (!(frum in obj)) continue
-
-    const to = renames[frum]
-
-    if (typeof to === 'object') {
-      obj[frum] = renameAll(to, obj[frum])
+  const newObj = {}
+  for (let prevKey in obj) {
+    const nextKey = renames[prevKey] || prevKey
+    if (typeof nextKey === 'object') {
+      newObj[prevKey] = renameAll(renames[prevKey], obj[prevKey])
     } else {
-      obj[to] = obj[frum]
-      delete obj[frum]
+      newObj[nextKey] = obj[prevKey]
     }
   }
-
-  return obj
+  return newObj
 }
 
 module.exports = curry(renameAll)
