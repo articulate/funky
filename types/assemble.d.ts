8a09a3619e5ccf5edc0b2f59f702a50d38e1f959
Markus Greystone <markus.greystone@gmail.com>
2023-12-11 19:19:05 -0500

    Working!

--- types/assemble.d.ts
+++ types/assemble.d.ts
@@ -2,32 +2,38 @@ import type { F } from 'ts-toolbelt'
 import type { UnionToIntersection } from 'type-fest'
 import type { Gt } from 'ts-arithmetic';
 
-type LastOf<T> = UnionToIntersection<T extends any ? () => T : never> extends () => (infer R) ? R : never
-type Push<T extends any[], V> = [...T, V];
-// TuplifyUnion and associated types courtesy of https://stackoverflow.com/a/55128956/20071103
-type TuplifyUnion<T, L = LastOf<T>, N = [T] extends [never] ? true : false> = true extends N ? [] : Push<TuplifyUnion<Exclude<T, L>>, L>;
+type Simplify<T> = T extends Record<string, any> ? { [K in keyof T]: T[K] } & {} : T;
 
 type _FillArray<Length extends number, T extends unknown, Acc extends unknown[] = []> = Acc['length'] extends Length ? Acc : _FillArray<Length, T, [...Acc, T]>;
 type FillArray<Length extends number, T extends unknown = unknown> = [Length] extends [never] ? [] : _FillArray<Length, T>;
 
-type _GetMaxPositiveNumber<Nums extends number[], CurrMax extends number = 0> =  Nums extends [infer First extends number, ...infer Rest extends number[]] ? _GetMaxPositiveNumber<Rest, Gt<First, CurrMax> extends 1 ? First : CurrMax> : CurrMax;
-type GetMaxPositiveNumber<Nums extends number[]> = Nums extends [] ? never : _GetMaxPositiveNumber<Nums>;
-
-type Simplify<T> = {
-    [K in keyof T]: T[K]
-} & {};
-
-
-type ZipTuples<Tuples extends unknown[]> =
-    TuplifyUnion<Tuples['length']> extends infer tuplifiedLengths extends number[]
-    ? FillArray<GetMaxPositiveNumber<tuplifiedLengths>> extends infer filledArray
-    ? {
-        [K in keyof filledArray]:
-        | (Tuples extends Tuples ? K extends keyof Tuples ? never : undefined : never)
-        | Simplify<UnionToIntersection<Tuples extends Tuples ? K extends keyof Tuples ? Tuples[K] : never : never>>
-    }
-    : never
+type GetLongestTuple<T extends any[], U extends any[] = T> = 
+    // Each "iteration" of nested distributivity filters out the shortest tuple, as its length will never be greater than the other tuples' lengths.
+    (
+        T extends T
+        ? U extends U
+        ? (
+            Gt<T['length'], U['length']> extends 1
+            ? T
+            : never
+        )
+        : never
+        : never
+    ) extends infer next extends any[]
+    ? (
+        // `next` will be `never` if `T` contains only 1 union member, as a tuple's length cannot be strictly greater than its own length (i.e. X['length'] ≯ X['length']).
+        [next] extends [never]
+        ? T // if so, return `T` (there may be other ways to terminate the recursion other than relying on an utterly useless final call, but this seems to work).
+        : GetLongestTuple<next> // otherwise continue.
+    )
     : never;
+
+type ZipTuples<Tuples extends any[]> = 
+    FillArray<GetLongestTuple<Tuples>['length']> extends infer filledArray
+    ? {
+        [K in keyof filledArray]: Simplify<UnionToIntersection<Tuples extends Tuples ? K extends keyof Tuples ? Tuples[K] : never : never>>
+    }
+    : []
 
 type Transforms = any[] | Record<PropertyKey, any>
 
@@ -45,9 +51,11 @@ type TransformArgs<T> =
             : never
 
 type AssembleArgs<T extends Transforms> =
-  [ TransformArgs<T> ] extends [ never ]
-    ? unknown[]
-    : ZipTuples<TransformArgs<T>>
+  TransformArgs<T> extends infer A
+    ? [A] extends [never]
+      ? unknown[]
+      : A extends any[] ? ZipTuples<A> : never
+    : never
 
 type Assembled<T> =
   T extends (...args: any[]) => any
