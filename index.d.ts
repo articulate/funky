2c4e4446d68275adca0341ee33ecfdb05754ac4e
Markus Greystone <markus.greystone@gmail.com>
2023-12-25 12:50:23 -0500

    assocWith

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
 export { default as all } from './types/all'
 export { default as assemble } from './types/assemble'
 export { default as assembleP } from './types/assembleP'
+export { default as assocWith } from './types/assocWith'
