ec57589ef8ba074092008c38a3c8cfb8e8560ed9
Markus Greystone <markus.greystone@gmail.com>
2023-12-25 12:13:04 -0500

    assembleP

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,3 @@
 export { default as all } from './types/all'
 export { default as assemble } from './types/assemble'
+export { default as assembleP } from './types/assembleP'
--- types/assemble.d.ts
+++ types/assemble.d.ts
@@ -1,56 +1,6 @@
 import type { F } from 'ts-toolbelt'
 
-type IntersectHead<T extends any[]> = (
-  T extends [infer F, ...any]
-    ? (x: F) => void
-    : T extends []
-    ? never
-    : (x: T[0]) => void
-) extends infer J
-  ? [J] extends [never]
-    ? never
-    : [J] extends [(x: infer V) => void]
-    ? V
-    : never
-  : never
-
-type Tail<T extends any[]> = T extends [any, ...infer R]
-  ? R
-  : T extends []
-  ? never
-  : T
-
-type NonRest<T extends any[]> = T extends [any, ...any]
-  ? unknown
-  : T extends []
-  ? unknown
-  : never
-
-type ZipArgs<T extends any[], A extends any[] = []> = Tail<T> extends never
-  ? A
-  : NonRest<T> extends never
-  ? [...A, ...IntersectHead<T>[]]
-  : ZipArgs<Tail<T>, [...A, IntersectHead<T>]>
-
-type CurriedParameters<T extends (...args: any[]) => any> = T extends F.Curry<
-  infer F
->
-  ? Parameters<F>
-  : Parameters<T>
-
-type XformArgs<T> = T extends (...args: any[]) => any
-  ? CurriedParameters<T>
-  : T extends []
-  ? never
-  : T extends [infer Head, ...infer Tail]
-  ? XformArgs<Head> | XformArgs<Tail>
-  : T extends any[]
-  ? { [K in keyof T]: XformArgs<T[K]> }[number]
-  : T extends Record<PropertyKey, any>
-  ? { [K in keyof T]: XformArgs<T[K]> }[keyof T]
-  : never
-
-type AssembleArgs<T> = ZipArgs<XformArgs<T>>
+import type { AssembleArgs } from './lib/assemble'
 
 type AssembleResult<T> = T extends (...args: any[]) => any
   ? ReturnType<T>
