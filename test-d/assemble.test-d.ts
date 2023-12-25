f4e76fb16c131c863faa1c7e988f9cecfae2afac
Markus Greystone <markus.greystone@gmail.com>
2023-12-24 19:23:45 -0500

    Even better

--- test-d/assemble.test-d.ts
+++ test-d/assemble.test-d.ts
@@ -2,36 +2,55 @@ import { add } from 'ramda'
 import { expectAssignable } from 'tsd'
 import { assemble } from '..'
 
-const xfrms1 = {
-  foo: [[ add(1) ]],
-  bar: [{ baz: add(2) }],
+const xfrmsUnary = {
+  foo: [[ add(1) ]] as [[ (x: number) => number ]],
+  bar: [{ baz: add(2) }] as [{ baz: (x: number) => number }],
   bat: 1
 } as const
 
-type Expectation1 = {
+type ExpectationUnary = {
   foo: [[ number ]],
   bar: [{ baz: number }],
   bat: number
 }
 
-expectAssignable<Expectation1>(assemble(xfrms1, 1))
-expectAssignable<Expectation1>(assemble(xfrms1)(1))
+expectAssignable<ExpectationUnary>(assemble(xfrmsUnary, 1))
+expectAssignable<ExpectationUnary>(assemble(xfrmsUnary)(1))
 
-const xfrms2 = {
-  foo: (one: string, two: string, three: string) => [ one, two ].join(','),
+const xfrmsNary = {
+  foo: (one: string, two: string) => [ one, two ].join(','),
   bar: {
     baz: (one: string, two: string, three: string) => [ one, two, three ].join('|'),
   },
   bat: 1
-} as const
+}
 
-type Expectation2 = {
+type ExpectationNary = {
   foo: string,
   bar: { baz: string },
   bat: number
 }
 
-expectAssignable<Expectation2>(assemble(xfrms2, 'one', 'two', 'three'))
-expectAssignable<Expectation2>(assemble(xfrms2)('one', 'two', 'three'))
-expectAssignable<Expectation2>(assemble(xfrms2)('one')('two', 'three'))
-expectAssignable<Expectation2>(assemble(xfrms2)('one')('two')('three'))
+expectAssignable<ExpectationNary>(assemble(xfrmsNary, 'one', 'two', 'three'))
+expectAssignable<ExpectationNary>(assemble(xfrmsNary)('one', 'two', 'three'))
+expectAssignable<ExpectationNary>(assemble(xfrmsNary)('one')('two', 'three'))
+expectAssignable<ExpectationNary>(assemble(xfrmsNary)('one')('two')('three'))
+
+const xfrms0ary = {
+  foo: (...params: string[]) => params.join(','),
+  bar: {
+    baz: (...params: string[]) => params.join('|'),
+  },
+  bat: 1
+}
+
+type Expectation0ary = {
+  foo: string,
+  bar: {
+    baz: string,
+  },
+  bat: number
+}
+
+expectAssignable<Expectation0ary>(assemble(xfrms0ary, 'one', 'two', 'three'))
+expectAssignable<Expectation0ary>(assemble(xfrms0ary)('one', 'two', 'three'))
--- types/assemble.d.ts
+++ types/assemble.d.ts
@@ -1,79 +1,74 @@
 import type { F } from 'ts-toolbelt'
-import type { UnionToIntersection } from 'type-fest'
-import type { Gt } from 'ts-arithmetic';
 
-type Simplify<T> = T extends Record<string, any> ? { [K in keyof T]: T[K] } & {} : T;
-
-type _FillArray<Length extends number, T extends unknown, Acc extends unknown[] = []> = Acc['length'] extends Length ? Acc : _FillArray<Length, T, [...Acc, T]>;
-type FillArray<Length extends number, T extends unknown = unknown> = [Length] extends [never] ? [] : _FillArray<Length, T>;
-
-type GetLongestTuple<T extends any[], U extends any[] = T> = 
-    // Each "iteration" of nested distributivity filters out the shortest tuple, as its length will never be greater than the other tuples' lengths.
-    (
-        T extends T
-        ? U extends U
-        ? (
-            Gt<T['length'], U['length']> extends 1
-            ? T
-            : never
-        )
-        : never
-        : never
-    ) extends infer next extends any[]
-    ? (
-        // `next` will be `never` if `T` contains only 1 union member, as a tuple's length cannot be strictly greater than its own length (i.e. X['length'] ≯ X['length']).
-        [next] extends [never]
-        ? T // if so, return `T` (there may be other ways to terminate the recursion other than relying on an utterly useless final call, but this seems to work).
-        : GetLongestTuple<next> // otherwise continue.
-    )
-    : never;
-
-type ZipTuples<Tuples extends any[]> = 
-    FillArray<GetLongestTuple<Tuples>['length']> extends infer filledArray
-    ? {
-        [K in keyof filledArray]: Simplify<UnionToIntersection<Tuples extends Tuples ? K extends keyof Tuples ? Tuples[K] : never : never>>
-    }
-    : []
-
-type Transforms = any[] | Record<PropertyKey, any>
-
-type TransformArgs<T> =
-  T extends (...args: any[]) => any
-    ? Parameters<T>
+type IntersectHead<T extends any[]> = (
+  T extends [infer F, ...any]
+    ? (x: F) => void
     : T extends []
-      ? never
-      : T extends readonly [infer I, ...infer R]
-        ? TransformArgs<I> | TransformArgs<R>
-        : T extends any[]
-          ? { [K in keyof T]: TransformArgs<T[K]> }[number]
-          : T extends Record<PropertyKey, any>
-            ? { [K in keyof T]: TransformArgs<T[K]> }[keyof T]
-            : never
-
-type AssembleArgs<T extends Transforms> =
-  TransformArgs<T> extends infer A
-    ? [A] extends [never]
-      ? unknown[]
-      : A extends any[] ? ZipTuples<A> : never
+    ? never
+    : (x: T[0]) => void
+) extends infer J
+  ? [J] extends [never]
+    ? never
+    : [J] extends [(x: infer V) => void]
+    ? V
     : never
+  : never
 
-type Assembled<T> =
-  T extends (...args: any[]) => any
-    ? ReturnType<T>
-    : T extends []
-      ? []
-      : T extends readonly [infer I, ...infer R]
-        ? [ Assembled<I>, ...Assembled<R> ]
-        : T extends any[]
-          ? { -readonly [K in keyof T]: Assembled<T[K]> }
-          : T extends Record<PropertyKey, any>
-            ? { -readonly [K in keyof T]: Assembled<T[K]> }
-            : T
+type Tail<T extends any[]> = T extends [any, ...infer R]
+  ? R
+  : T extends []
+  ? never
+  : T
 
-export default function assemble
-  <const T extends Transforms>(
-    xfrms: T, ...args: AssembleArgs<T>,
-  ): Assembled<T>
+type NonRest<T extends any[]> = T extends [any, ...any]
+  ? unknown
+  : T extends []
+  ? unknown
+  : never
 
-export default function assemble
-  <const T extends Transforms>(xfrms: T): F.Curry<(...args: AssembleArgs<T>) => Assembled<T>>
+type ZipArgs<T extends any[], A extends any[] = []> = Tail<T> extends never
+  ? A
+  : NonRest<T> extends never
+  ? [...A, ...IntersectHead<T>[]]
+  : ZipArgs<Tail<T>, [...A, IntersectHead<T>]>
+
+type CurriedParameters<T extends (...args: any[]) => any> = T extends F.Curry<
+  infer F
+>
+  ? Parameters<F>
+  : Parameters<T>
+
+type XformArgs<T> = T extends (...args: any[]) => any
+  ? CurriedParameters<T>
+  : T extends []
+  ? never
+  : T extends [infer Head, ...infer Tail]
+  ? XformArgs<Head> | XformArgs<Tail>
+  : T extends any[]
+  ? { [K in keyof T]: XformArgs<T[K]> }[number]
+  : T extends Record<PropertyKey, any>
+  ? { [K in keyof T]: XformArgs<T[K]> }[keyof T]
+  : never
+
+type AssembleArgs<T> = ZipArgs<XformArgs<T>>
+
+type AssembleResult<T> = T extends (...args: any[]) => any
+  ? ReturnType<T>
+  : T extends []
+  ? []
+  : T extends [infer Head, ...infer Tail]
+  ? [AssembleResult<Head>, ...AssembleResult<Tail>]
+  : T extends any[]
+  ? { [K in keyof T]: AssembleResult<T[K]> }
+  : T extends Record<PropertyKey, any>
+  ? { [K in keyof T]: AssembleResult<T[K]> }
+  : T
+
+export default function assemble<T>(
+  xfrms: T,
+): F.Curry<(...args: AssembleArgs<T>) => AssembleResult<T>>
+
+export default function assemble<T>(
+  xfrms: T,
+  ...args: AssembleArgs<T>
+): AssembleResult<T>
